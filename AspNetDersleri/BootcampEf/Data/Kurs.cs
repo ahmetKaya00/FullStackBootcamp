bccc4d9782122c0e3b203b56313213fd23c157f8
Ahmet Kaya <115210242+ahmetKaya00@users.noreply.github.com>
2023-12-13 15:04:32 +0300

    Proje Restore

--- AspNetDersleri/BootcampEf/Data/Kurs.cs
+++
@@ -1,8 +0,0 @@
-namespace BootcampEf.Data
-{
-    public class Kurs{
-        public int KursId { get; set; }
-        public string? Baslik { get; set; }
-        public ICollection<KursKayit> KursKayitlari {get;set;} = new List<KursKayit>();
-    }
-}
